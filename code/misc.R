e8ff433dd5ce1655412f8c1ac9fd0d20382987d8
Peter Carbonetto <pcarbo@uchicago.edu>
2015-06-27 10:10:17 -0700

    Removed the "len" function since it is not used either.

--- code/misc.R
+++ code/misc.R
@@ -6,15 +6,6 @@ cp0 <- function (...)
   cat(paste0(...))
 
 # ----------------------------------------------------------------------
-# Computes the length of a vector with the option of removing NA's.
-len <- function(x, na.rm = TRUE) {
-  if (na.rm)
-    sum(!is.na(x))
-  else
-    length(x)
-}
-
-# ----------------------------------------------------------------------
 # For each row of the matrix or data frame, returns true if all the
 # entries in the row are provided (not missing).
 none.missing.row <- function (x)
