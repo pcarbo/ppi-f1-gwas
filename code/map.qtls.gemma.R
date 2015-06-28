03593b360f7f8073a92529ca815e3e49a5c5e64b
Peter Carbonetto <pcarbo@uchicago.edu>
2015-06-27 17:52:43 -0700

    Tidied up map.qtls.gemma.R.

--- code/map.qtls.gemma.R
+++ code/map.qtls.gemma.R
@@ -1,58 +1,54 @@
 # Maps QTLs for phenotypes measured in wild-type F1 mice using a linear
 # mixed model (GEMMA) that corrects for possible confounding to due
-# relatedness
+# relatedness.
 
-# Load packages
+# Load packages.
 require(data.table)
 require(qtl)
 
- # Set location of dropbox files
-dropbox.dir <- "/home/palmerlab/Dropbox (Palmer Lab)/Palmer Lab/Kyle Engel/data_analysis/"
-setwd(dropbox.dir)
+# Load some extra functions definitions.
+source("read.data.R")
+source("data.manip.R")
+source("functions.transform.R")
+source("functions.gemma.R")
+source("misc.R")
 
-# Load some extra functions
-source("code/read.data.R")
-source("code/data.manip.wtcombined.R")
-source("code/transformation.functions.R")
-source("code/wtcombined.gemma.functions.R")
-source("code/misc.R")
+# Load the model defaults.
+source("wtdefaults.v3.R")
 
-# Set location of data files, gemma executable, and the default model information
-pheno.filename <- "data/combined.wts.csv"
-geno.filename  <- "data/mda.F1.lg.csv"
-gemma.exe      <- "/usr/local/src/gemma/bin/gemma"
-source("defaults/wtdefaults.version3.R")
-
-system("mkdir ~/Desktop/gemma_output")
+# Set location of the data files and the gemma executable.
+pheno.filename <- "../data/pheno.csv"
+geno.filename  <- "../data/mda.F1.csv"
+gemma.exe      <- "~/bin/gemma"
 
 for (which.analysis in names(model.info)) {
   
-  # Since this is on wtcombined data, we only want WTs
+  # We only want the wild-types.
   choose.samples <- "wt"
   
   # Create folders for GEMMA and specify some model information
-  system(paste0("mkdir ~/Desktop/gemma_output/", which.analysis))
-  gemmadir       <- paste0("~/Desktop/gemma_output/", which.analysis, "/", choose.samples)
-  system(paste0("mkdir ", gemmadir))
+  system(paste0("mkdir ~/gemma_output/",which.analysis))
+  gemmadir <- paste0("~/gemma_output/",which.analysis,"/",choose.samples)
+  system(paste0("mkdir ",gemmadir))
   
-  # Name of final result RData file
-  resultsfile <- paste0("~/Dropbox (Palmer Lab)/Palmer Lab/Kyle Engel/wt_combined_gemma/",
-                        which.analysis, ".wtcombined.RData")
+  # Name of final result .RData file.
+  resultsfile <-
+    paste0("~/wt_combined_gemma/",which.analysis,".wtcombined.RData")
   
-  # A few more specifications
+  # A few more specifications.
   use.kinship <- TRUE
   seed        <- 1
   
-  # Print out status
-  cp0("On ", which.analysis, " and ", choose.samples, "\n")
+  # Print out the status.
+  cp0("On ",which.analysis," and ",choose.samples,"\n")
   
-  # Extract information out of the model.info list
-  analysis         <- model.info[[which.analysis]]
-  phenotype        <- which.analysis
-  transformation   <- analysis$transformation
-  covariates       <- unique(analysis$covariates)
-  outliers         <- analysis$outlier.function
-  strain.outliers  <- unique(analysis$outliers)
+  # Extract information out of model.info list.
+  analysis        <- model.info[[which.analysis]]
+  phenotype       <- which.analysis
+  transformation  <- analysis$transformation
+  covariates      <- unique(analysis$covariates)
+  outliers        <- analysis$outlier.function
+  strain.outliers <- unique(analysis$outliers)
 
   # LOAD PHENOTYPE DATA
   # -------------------
@@ -60,24 +56,24 @@ for (which.analysis in names(model.info)) {
   
   # Read in the data, transform it if necessary, and remove outliers
   pheno <- read.combined.wt(pheno.filename)
-  pheno <- transform.pheno(pheno, phenotype, transformation)
-  pheno <- remove.outliers(pheno, phenotype, covariates, outliers, strain.outliers)
+  pheno <- transform.pheno(pheno,phenotype,transformation)
+  pheno <- remove.outliers(pheno,phenotype,covariates,outliers,strain.outliers)
   
-  # Remove MA strain (no genotype data is available for MA)
-  pheno        <- subset(pheno, strain != "MA")
-  pheno        <- pheno[order(pheno$strain), ]
+  # Remove MA strain (since no genotype data is available for MA).
+  pheno        <- subset(pheno,strain != "MA")
+  pheno        <- pheno[order(pheno$strain),]
   pheno$strain <- droplevels(pheno$strain)
   
   # Only include samples in the analysis for which the phenotype and all
-  # covariates are observed
+  # covariates are observed.
   cols  <- c(phenotype,covariates)
   rows  <- which(none.missing.row(pheno[cols]))
-  pheno <- pheno[rows, ]
+  pheno <- pheno[rows,]
   
   # Print out some more information  
-  cat("Including all (", nrow(pheno), ") WT samples in analysis.\n", sep="")
+  cat("Including all (",nrow(pheno),") WT samples in analysis.\n",sep="")
   
-  # Convert the sex, time of day, and study columns to binary
+  # Convert the sex, time of day, and study columns to binary values.
   levels(pheno$sex)         <- 0:1
   levels(pheno$fctimeofday) <- 0:1
   levels(pheno$study)       <- 0:1
@@ -93,22 +89,22 @@ for (which.analysis in names(model.info)) {
   rownames(map) <- NULL
   rm(d)
   
-  # Drop sex-linked and mitochondrial DNA genotypes
+  # Drop sex-linked and mitochondrial DNA genotypes.
   markers <- which(is.element(map$chr,1:19))
-  map     <- transform(map[markers, ], chr = droplevels(chr))
-  geno    <- geno[, markers]
+  map     <- transform(map[markers,],chr = droplevels(chr))
+  geno    <- geno[,markers]
   
   # Also, drop SNPs that are polymorphic in 5 strains or less, because
   # it is unlikely that we will discover phenotype associations with
-  # these SNPs
-  markers <- which(pmin(colSums(geno), colSums(1 - geno)) > 5)
-  map     <- map[markers, ]
-  geno    <- geno[, markers]
+  # these SNPs.
+  markers <- which(pmin(colSums(geno),colSums(1 - geno)) > 5)
+  map     <- map[markers,]
+  geno    <- geno[,markers]
   
-  # Align the phenotypes and genotypes
-  geno <- geno[match(pheno$strain, rownames(geno)), ]
+  # Align the phenotypes and genotypes.
+  geno <- geno[match(pheno$strain,rownames(geno)),]
   
-  # Initialize the random number generator
+  # Initialize the random number generator.
   set.seed(seed)
 
   # MAP QTLs USING GEMMA
@@ -116,22 +112,19 @@ for (which.analysis in names(model.info)) {
   if (use.kinship) {
 
     # Map QTLs using a separate kinship matrix for each chromosome
-    out <- run.gemma(phenotype, covariates, pheno, geno, map, gemmadir, gemma.exe)
+    out <- run.gemma(phenotype,covariates,pheno,geno,map,gemmadir,gemma.exe)
     gwscan.gemma <- out$gwscan
     pve.gemma    <- out$pve
-    
   } else {
 
-    # Maps QTLs without fully accounting for population structure
+    # Map QTLs without fully accounting for population structure.
     pve.gemma    <- NULL
-    gwscan.gemma <- run.gemma.norr(phenotype, covariates, pheno, geno, map,
-                                   gemmadir, gemma.exe)
-    
+    gwscan.gemma <- run.gemma.norr(phenotype,covariates,pheno,geno,map,
+                                   gemmadir,gemma.exe)
   }
 
-  # Save results to file
+  # Save results to file.
   cat("Saving results to file.\n")
-  save(list = c("analysis", "choose.samples", "map", "gwscan.gemma", "pve.gemma"),
+  save(list = c("analysis","choose.samples","map","gwscan.gemma","pve.gemma"),
        file = resultsfile)
-
 }
